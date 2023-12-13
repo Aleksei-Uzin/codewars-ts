ffff74ac5d489c69613100a9df7cd6aa7c9cbabc
Aleksei-Uzin <aleksei.uzin@gmail.com>
2023-12-13 16:36:28 +0400

    solution

---
+++ 8-kyu/FIXME_Replace_all_dots.ts
@@ -0,0 +1,9 @@
+/**
+ * FIXME: Replace all dots
+ * https://www.codewars.com/kata/596c6eb85b0f515834000049/train/typescript
+ *
+ */
+
+export function replaceDots(str: string): string {
+  return str.replace(/\./g, '-')
+}
